6b0c5bcd7ee78ccd10c87def7aea4d18b8aeb583
andresfb <andres@bastidas.us>
2023-07-16 12:31:16 -0400

    Added some extra test to the Feed Reader to see what fields can I use

--- app/Console/Commands/TestAppCommand.php
+++ app/Console/Commands/TestAppCommand.php
@@ -44,22 +44,28 @@ class TestAppCommand extends Command
 ////            $r = $f->read('https://www.metacritic.com/rss');
 ////            $r = $f->read('https://feeds.npr.org/1001/rss.xml');
 ////            $r = $f->read('https://wsvn.com/feed/');
+////            $r = $f->read('https://www.youtube.com/feeds/videos.xml?channel_id=UCD88k6KX9chc-KgEOuSJSVw');
 //
 //            echo $r->get_title() . PHP_EOL . PHP_EOL;
 //
 //            foreach ($r->get_items() as $item) {
 //                echo "Title: " . $item->get_title() . PHP_EOL . PHP_EOL;
-//                echo "Description: " . $item->get_description() . PHP_EOL . PHP_EOL;
-//                echo "Link: " . $item->get_permalink() . PHP_EOL . PHP_EOL;
 //                echo "Content: " . $item->get_content() . PHP_EOL . PHP_EOL;
+//                echo "Description: " . $item->get_description() . PHP_EOL . PHP_EOL;
+//                echo "Enclosure Description: " . $item->get_enclosure()?->description . PHP_EOL . PHP_EOL;
+//                echo "Link: " . $item->get_permalink() . PHP_EOL . PHP_EOL;
 //                if (is_array($item->get_thumbnail())) {
 //                    echo print_r($item->get_thumbnail(), true) . PHP_EOL . PHP_EOL;
 //                } else {
 //                    echo "Thumbnail: " . $item->get_thumbnail() . PHP_EOL . PHP_EOL;
 //                }
-//                echo "Base: " . $item->get_base() . PHP_EOL . PHP_EOL;
-//                echo "Source: " . $item->get_source() . PHP_EOL . PHP_EOL;
+//                echo "Enclosure Thumbnails: " . print_r($item->get_enclosure()?->thumbnails, true) . PHP_EOL . PHP_EOL;
+//                echo "Date: " . $item->get_date('Y-m-d H:i:s') . PHP_EOL . PHP_EOL;
+////                echo "Enclosures: " . print_r($item->get_enclosures(), true) . PHP_EOL . PHP_EOL;
+//                echo "Data: " . print_r($item->data, true) . PHP_EOL . PHP_EOL;
+//
 //                $this->line("\n");
+//                break;
 //            }
 //
 //            $this->info(count($r->get_items()));
